004e0641d26025004048ba237b6ccc621a8829c0
Akarsh Barar <barar.akarsh8@gmail.com>
2020-08-05 01:53:50 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Spotify clone</h1>
     </div>
   );
 }
 
 export default App;
+//https://developer.spotify.com/dashboard/applications/b57e953002714d9187c5b974067a7902
